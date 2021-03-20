17f963c4e91b6bb6050b7879bce1c6533be8735c
tanishaCodes <thowell.dev@gmail.com>
2021-03-20 19:28:01 -0400

    updated footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,9 +7,9 @@ import Col from 'react-bootstrap/Col';
 function Footer() {
     return(
         <footer className='mt-5'>
-            <Container className='lead font-weight-light' fluid={true}>
+            <Container className='lead font-weight-light' fluid='md'>
                 <Row className='border-top justify-content-center p-3'>
-                    <Col xs={1} sm={12} md={6} lg={12} />
+                    <Col xs={1} sm={12} md={0} lg={12} />
                         &#169;2020 Made by Tanisha Howell
                 </Row>
             </Container>
